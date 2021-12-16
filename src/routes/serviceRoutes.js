29027dae7a466a6088b154f3bf4d78265cb1c0ba
paulorojas <paulo.rojas.perez@ciisa.cl>
2021-12-16 00:39:47 -0300

    cretae service

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -7,7 +7,7 @@ import { verifyToken, isAdmin } from '../middlewares/authJwt.js'
 
 router.get('/service', serviceCtrl.getService);
 
-router.post('/service', verifyToken, isAdmin, serviceCtrl.createService);
+router.post('/service', serviceCtrl.createService);
 
 router.put('/service/:id', verifyToken, isAdmin, serviceCtrl.updateServiceById);
 
